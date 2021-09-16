e6e774b9470543963e7436231f4ab456390f5407
honghuan.Liu <honghuan0921@gmail.com>
2021-09-16 09:44:42 +0800

    init springboot

---
+++ src/test/java/cc/honghuan/redis/tool/RedisToolApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.honghuan.redis.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
